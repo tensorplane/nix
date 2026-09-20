aa107fc10673efd69455af03923330c6160173f2
sebastian <silenteh@gmail.com>
2026-09-20 17:00:02 +0000

    test: repair current nightly CI prerequisites

--- src/sys/uio.rs
+++ src/sys/uio.rs
@@ -52,7 +52,7 @@ pub fn readv<Fd: AsFd>(fd: Fd, iov: &mut [IoSliceMut<'_>]) -> Result<usize> {
 /// Buffers in `iov` will be written in order until all buffers have been written
 /// or an error occurs. The file offset is not changed.
 ///
-/// See also: [`writev`](fn.writev.html) and [`pwrite`](fn.pwrite.html)
+/// See also: [`writev`] and [`pwrite`]
 #[cfg(not(any(target_os = "redox", target_os = "haiku", target_os = "solaris", target_os = "cygwin")))]
 pub fn pwritev<Fd: AsFd>(
     fd: Fd,
@@ -81,7 +81,7 @@ pub fn pwritev<Fd: AsFd>(
 /// no more bytes are available, or an error occurs. The file offset is not
 /// changed.
 ///
-/// See also: [`readv`](fn.readv.html) and [`pread`](fn.pread.html)
+/// See also: [`readv`] and [`pread`]
 #[cfg(not(any(target_os = "redox", target_os = "haiku", target_os = "solaris", target_os = "cygwin")))]
 // Clippy doesn't know that we need to pass iov mutably only because the
 // mutation happens after converting iov to a pointer
@@ -147,8 +147,8 @@ pub fn pread<Fd: AsFd>(fd: Fd, buf: &mut [u8], offset: off_t) -> Result<usize> {
 /// This is the same underlying C structure as `IoSlice`,
 /// except that it refers to memory in some other process, and is
 /// therefore not represented in Rust by an actual slice as `IoSlice` is. It
-/// is used with [`process_vm_readv`](fn.process_vm_readv.html)
-/// and [`process_vm_writev`](fn.process_vm_writev.html).
+/// is used with [`process_vm_readv`]
+/// and [`process_vm_writev`].
 #[cfg(linux_android)]
 #[repr(C)]
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq)]
